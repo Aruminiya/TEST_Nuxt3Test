6fdd10f60b6e67e6204fa2207b4158ea3e031469
“Leo” <leo80103@gmail.comgit config --global user.email leo80103@gmail.com>
2024-06-14 16:37:11 +0800

    側邊欄位切版測試

--- app.vue
+++ app.vue
@@ -1,6 +1,19 @@
 <template>
   <NuxtLayout>
-    <NuxtPage />
+    <div class="container mx-auto">
+      <div class="flex flex-wrap">
+        <div class="w-full sm:w-3/12">側邊欄位</div>
+        <div class="w-full sm:w-9/12">
+          <NuxtPage />
+        </div>
+      </div>
+    </div>
+    <div class="relative">
+      <div class="absolute left-0 w-screen">
+        <div class="bg-gray-200 p-4">none container</div>
+      </div>
+    </div>
+
   </NuxtLayout>
 </template>
 
